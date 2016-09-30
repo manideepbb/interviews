9c5446100cc2b2c7807bfb1e7bdc923d69db78bc
manideepbb <manideep.bb@gmail.com>
2016-09-30 10:33:21 -0500

    Update Exercise5.java

--- java/src/main/java/com/elsevier/education/Exercise5.java
+++ java/src/main/java/com/elsevier/education/Exercise5.java
@@ -4,17 +4,34 @@ package com.elsevier.education;
 
 TODO: Turn the "Singleton" class into an actual singleton. The main() method should still call .doSomething().
 
+Create a private static instance 
+make constructor private
+create a public getSingleton() method that returns the object of singleton
 */
-public class Exercise5 {
-	
-	public static class Singleton {
+
+
+public static class Singleton {
+
+	private static Singleton s = null;
+	private Singleton()
+	{
+	}
+	public static Singleton getSingleton()
+	{
+		if (s==null)
+		{
+			s=new Singleton();
+		}
+		return s;
+	}
+		
 		public void doSomeThing() {
 			System.out.println("Doing something....");
 	    }
 	}
-		
-	public static void main(String a[]){
-		Singleton st = new Singleton();
+
+
+public static void main(String a[]){
+		Singleton st = Singleton.getSingleton();
 		st.doSomeThing();
 	}
-}
